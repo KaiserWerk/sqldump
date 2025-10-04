1e72eb75d81eeb8ab826867ee525f0a4ad0bb3a6
Robin K. <web@r-k.mx>
2025-10-04 03:41:49 +0200

    added alternative MySQL function to call with DSN

--- mysql.go
+++ mysql.go
@@ -8,15 +8,35 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func GetMySQLBackupFile(username, password, hostname, port, dbname string) func() (string, error) {
+func GetMySQLBackupFile(username, password, hostname, port, dbname, filebasename string) func() (string, error) {
 	return func() (string, error) {
-		dumpFilenameFormat := dbname
 		db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8", username, password, hostname, port, dbname))
 		if err != nil {
 			return "", err
 		}
 
-		dumper, err := mysqldump.Register(db, "./", dumpFilenameFormat)
+		dumper, err := mysqldump.Register(db, "./", filebasename)
+		if err != nil {
+			return "", err
+		}
+
+		resultFilename, err := dumper.Dump()
+		if err != nil {
+			return "", err
+		}
+
+		return resultFilename, nil
+	}
+}
+
+func GetMySQLBackupFileByDSN(dsn string, filebasename string) func() (string, error) {
+	return func() (string, error) {
+		db, err := sql.Open("mysql", dsn)
+		if err != nil {
+			return "", err
+		}
+
+		dumper, err := mysqldump.Register(db, "./", filebasename)
 		if err != nil {
 			return "", err
 		}
